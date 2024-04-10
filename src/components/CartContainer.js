0dd19225476da4aa16d5384b6718e80e23eec774
Muhammad Turkmen <muhammadturkmen204@gmail.com>
2024-04-10 19:09:32 +0430

    init

--- src/components/CartContainer.js
+++ src/components/CartContainer.js
@@ -4,6 +4,18 @@ import { useSelector } from "react-redux/es/hooks/useSelector";
 
 const CartContainer = () => {
   const { cartItems, total, amount } = useSelector((store) => store.cart);
+
+  if (amount < 1) {
+    return (
+      <section className="cart">
+        <header>
+          <h2>your bag</h2>
+          <h4 className="empty-cart">is currently empty</h4>
+        </header>
+      </section>
+    );
+  }
+
   return <div>CartContainer</div>;
 };
 
