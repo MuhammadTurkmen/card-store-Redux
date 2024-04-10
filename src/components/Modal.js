a5ed2e64c8ec84945e152d7568347a9150ab52ac
Muhammad Turkmen <muhammadturkmen204@gmail.com>
2024-04-10 21:04:11 +0430

    init

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,7 +1,13 @@
 import React from "react";
 
 const Modal = () => {
-  return <aside className="modal-container">Modal</aside>;
+  return (
+    <aside className="modal-container">
+      <div className="modal">
+        <h4>remove all items from shopping card?</h4>
+      </div>
+    </aside>
+  );
 };
 
 export default Modal;
