69bf3b93a8c99e52f7bfbdf8cdfdb37c7c946345
Muhammad Turkmen <muhammadturkmen204@gmail.com>
2024-04-10 21:13:45 +0430

    init

--- src/store.js
+++ src/store.js
@@ -1,5 +1,6 @@
 import { configureStore } from "@reduxjs/toolkit";
 import cartReducer from "./features/cart/cartSlice";
+import modalReducer from "./features/modal/modalSlice";
 
 export const store = configureStore({
   reducer: {
