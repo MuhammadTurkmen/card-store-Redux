dff973f456155a62c8781047a47668b387e5a0ba
Muhammad Turkmen <muhammadturkmen204@gmail.com>
2024-04-10 21:05:49 +0430

    init

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -9,6 +9,9 @@ const Modal = () => {
           <button type="button" className="btn confirm-btn">
             confirm
           </button>
+          <button type="button" className="btn clear-btn">
+            cancel
+          </button>
         </div>
       </div>
     </aside>
