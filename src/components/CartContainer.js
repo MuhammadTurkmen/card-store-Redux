adb8cd8d4971718a88072f8369a28cc93f53895b
Muhammad Turkmen <muhammadturkmen204@gmail.com>
2024-04-10 19:08:06 +0430

    init

--- src/components/CartContainer.js
+++ src/components/CartContainer.js
@@ -3,6 +3,7 @@ import CartItem from "./CartItem";
 import { useSelector } from "react-redux/es/hooks/useSelector";
 
 const CartContainer = () => {
+  const { cartItems, total, amount } = useSelector((store) => store.cart);
   return <div>CartContainer</div>;
 };
 
