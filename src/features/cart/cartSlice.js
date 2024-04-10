cba65e5f88fb65307de3aa923e6d05c69a5c647a
Muhammad Turkmen <muhammadturkmen204@gmail.com>
2024-04-10 20:27:24 +0430

    init

--- src/features/cart/cartSlice.js
+++ src/features/cart/cartSlice.js
@@ -31,6 +31,6 @@ const cartSlice = createSlice({
 });
 
 // console.log(cartSlice);
-export const { clearCart, removeItem } = cartSlice.actions;
+export const { clearCart, removeItem, increase, decrease } = cartSlice.actions;
 
 export default cartSlice.reducer;
