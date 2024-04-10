ce7a0151a9165eb7d6ec8a4a8c4223806b7d87f2
Muhammad Turkmen <muhammadturkmen204@gmail.com>
2024-04-10 21:19:40 +0430

    init

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import Modal from "./components/Modal";
 
 function App() {
   const { cartItems } = useSelector((store) => store.cart);
+  const { isOpen } = useSelector((store) => store.modal);
   const dispatch = useDispatch();
 
   useEffect(() => {
