088a0c8172bf807c85fa0848fa3963037c69e5ff
Muhammad Turkmen <muhammadturkmen204@gmail.com>
2024-04-10 19:25:30 +0430

    init

--- src/components/CartItem.js
+++ src/components/CartItem.js
@@ -14,7 +14,7 @@ export const CartItem = ({ id, img, title, price, amount }) => {
         <button className="amount-btn">
           <ChevronUp />
         </button>
-
+        <p className="amount">{amount}</p>
         <button className="amount-btn">
           <ChevronDown />
         </button>
