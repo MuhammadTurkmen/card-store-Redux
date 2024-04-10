f1739e7e6d0d8cc5e5ab3eb337c9aaa43d78804f
Muhammad Turkmen <muhammadturkmen204@gmail.com>
2024-04-10 18:41:28 +0430

    init

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,2 +1,14 @@
 import { CartIcon } from "../icons";
 import { UseSelector } from "react-redux/es/hooks/useSelector";
+
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <nav>
+      <div className="nav-center"></div>
+    </nav>
+  );
+};
+
+export default Navbar;
