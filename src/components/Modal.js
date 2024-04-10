78a0d43ac84fdd78ea24e476b8492310596d70df
Muhammad Turkmen <muhammadturkmen204@gmail.com>
2024-04-10 21:28:57 +0430

    init

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -24,7 +24,7 @@ const Modal = () => {
             type="button"
             className="btn clear-btn"
             onClick={() => {
-              dispatch(closeModal);
+              dispatch(closeModal());
             }}
           >
             cancel
