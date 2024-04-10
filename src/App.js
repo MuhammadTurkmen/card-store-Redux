3b4ba36f680730ece6a10d21f5a2a49410abdabc
Muhammad Turkmen <muhammadturkmen204@gmail.com>
2024-04-10 20:55:45 +0430

    init

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import Navbar from "./components/Navbar";
 import CartContainer from "./components/CartContainer";
+import { useDispatch } from "react-redux";
+import { useSelector } from "react-redux/es/hooks/useSelector";
 
 function App() {
   return (
