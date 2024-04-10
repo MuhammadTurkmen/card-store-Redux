59fbbbb49d47f05edb1c9a7ec34504f90a238071
Muhammad Turkmen <muhammadturkmen204@gmail.com>
2024-04-10 14:58:55 +0430

    init

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-import './index.css';
-import App from './App';
+import React from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import { store } from "./store";
+import { Provider } from "react-redux";
 
-const container = document.getElementById('root');
+const container = document.getElementById("root");
 const root = createRoot(container);
 
 root.render(
