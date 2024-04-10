6708f57f87417e67f213439c74218769bfdc9e9b
Muhammad Turkmen <muhammadturkmen204@gmail.com>
2024-04-10 19:22:14 +0430

    init

--- src/components/CartItem.js
+++ src/components/CartItem.js
@@ -5,6 +5,9 @@ export const CartItem = ({ id, img, title, price, amount }) => {
   return (
     <article className="cart-item">
       <img src={img} alt={title} />
+      <div>
+        <h4>{title}</h4>
+      </div>
     </article>
   );
 };
