3f35c91572f18e77921a88918b597c0f89e15402
Muhammad Turkmen <muhammadturkmen204@gmail.com>
2024-04-10 20:57:40 +0430

    init

--- src/App.js
+++ src/App.js
@@ -3,10 +3,14 @@ import CartContainer from "./components/CartContainer";
 import { useDispatch } from "react-redux";
 import { useSelector } from "react-redux/es/hooks/useSelector";
 import { calculateTotals } from "./features/cart/cartSlice";
+import { useEffect } from "react";
 
 function App() {
   const { cartItems } = useSelector((store) => store.cart);
   const dispatch = useDispatch();
+
+  useEffect(() => {}, [cartItems]);
+
   return (
     <main>
       <Navbar />
