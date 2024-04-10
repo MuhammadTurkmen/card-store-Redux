8448c753dec0f41e12eaa6ac76449196da8595fb
Muhammad Turkmen <muhammadturkmen204@gmail.com>
2024-04-10 16:27:30 +0430

    init

---
+++ src/features/cart/cartSlice.js
@@ -0,0 +1,11 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  cartItems: [],
+  amount: 0,
+};
+
+const cartSlice = createSlice({
+  name: "cart",
+  initialState,
+});
