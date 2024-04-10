cd396acaee7c62cdc37ae52e78941737aa438fb7
Muhammad Turkmen <muhammadturkmen204@gmail.com>
2024-04-10 21:28:12 +0430

    init

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -10,7 +10,14 @@ const Modal = () => {
       <div className="modal">
         <h4>remove all items from shopping card?</h4>
         <div className="btn-container">
-          <button type="button" className="btn confirm-btn">
+          <button
+            type="button"
+            className="btn confirm-btn"
+            onClick={() => {
+              dispatch(clearCart());
+              dispatch(closeModal());
+            }}
+          >
             confirm
           </button>
           <button type="button" className="btn clear-btn">
