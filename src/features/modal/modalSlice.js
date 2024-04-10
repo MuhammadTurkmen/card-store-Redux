583112b8a1975d821791f660a4b071074a7f8db3
Muhammad Turkmen <muhammadturkmen204@gmail.com>
2024-04-10 21:10:54 +0430

    init

---
+++ src/features/modal/modalSlice.js
@@ -0,0 +1,15 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  isOpen: false,
+};
+
+const modalSlice = createSlice({
+  name: "modal",
+  initialState,
+  reducers: {
+    openModal: (state, action) => {
+      state.isOpen = true;
+    },
+  },
+});
