edbbe1ed7abdcb526d5e89587e544266d19e9284
Muhammad Turkmen <muhammadturkmen204@gmail.com>
2024-04-10 20:56:10 +0430

    init

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import Navbar from "./components/Navbar";
 import CartContainer from "./components/CartContainer";
 import { useDispatch } from "react-redux";
 import { useSelector } from "react-redux/es/hooks/useSelector";
+import { calculateTotals } from "./features/cart/cartSlice";
 
 function App() {
   return (
