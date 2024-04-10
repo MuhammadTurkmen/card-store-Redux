b1b669d64ca20758ffe793aa4ea2626fdd6e5caa
Muhammad Turkmen <muhammadturkmen204@gmail.com>
2024-04-10 20:28:09 +0430

    init

--- src/components/CartItem.js
+++ src/components/CartItem.js
@@ -1,5 +1,5 @@
 import { ChevronDown, ChevronUp } from "../icons";
-import { removeItem } from "../features/cart/cartSlice";
+import { removeItem, increase, decrease } from "../features/cart/cartSlice";
 import { useDispatch } from "react-redux";
 import React from "react";
 
